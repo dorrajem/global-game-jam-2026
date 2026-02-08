4e3e7d532e0784e4e84b7f21eb8c88f133c69421
dali-333 <abdellatifmouhamedali@gmail.com>
2026-02-08 14:48:08 +0100

    idk

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
+
 @onready var start_button: Button = $VBoxContainer/StartButton
 @onready var exit_button: Button = $VBoxContainer/ExitButton
+@onready var tutorial_button: Button = $TutorialButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,3 +20,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu/tutorial.tscn")
